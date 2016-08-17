d0890edc789573a43b679e5ccfc27f52bd41b83d
Jonathan Soo <jcsoo@agora.com>
2016-08-17 11:55:23 -0400

    Add single-use Resolver

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,57 @@ mod dns_query;
 
 use trust_dns::udp::UdpClientConnection;
 use trust_dns::rr::record_data::RData;
+use trust_dns::op::Message;
 
 use futures::*;
 use futures::stream::Stream;
-use futures_mio::Loop;
+use futures_io::IoFuture;
+use futures_mio::{Loop, LoopHandle, UdpSocket};
 
 use std::net::SocketAddr;
+use std::io;
 
-macro_rules! t {
-    ($e:expr) => (match $e {
-        Ok(e) => e,
-        Err(e) => panic!("{} failed with {:?}", stringify!($e), e),
-    })
+pub struct Resolver {
+    socket: UdpSocket,
+    addr: SocketAddr,
+}
+
+impl Resolver {
+    pub fn new(lh: LoopHandle, addr: SocketAddr) -> BoxFuture<Resolver, io::Error> {
+        let local_addr = "0.0.0.0:0".parse::<SocketAddr>().unwrap();
+        let socket_bind = lh.udp_bind(&local_addr); // Future<Item = UdpSocket, Error = Error>
+        socket_bind.map(move |socket| { 
+            Resolver {
+                socket: socket,
+                addr: addr
+            }
+        }).boxed()
+    }
+
+    pub fn resolve(self, host: &str) -> BoxFuture<Message, (io::Error, UdpSocket)> {
+        let buf = dns_query::build_query(1234, host);
+        let addr = self.addr;
+        
+        self.socket
+            .into_future()
+            .and_then(move |(ar, a)| {
+                let ar = ar.unwrap(); // Ready
+                assert!(ar.is_write());
+
+                a.send_to(&buf, &addr).unwrap();
+                a.into_future() // Future<Item = (Option<Ready>, UdpSocket)>
+            })
+            .and_then(|(ar,a)| {
+                let ar = ar.unwrap(); // Ready
+                assert!(ar.is_read());
+
+                let mut buf = [0; 512];
+                let (_size, _addr) = a.recv_from(&mut buf).unwrap();
+
+                Ok(dns_query::parse_response(&mut buf))
+            }).boxed()
+            //.map_err(|(e, _)| e).boxed()
+       }
 }
 
 fn main() {
