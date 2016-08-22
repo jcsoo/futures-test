83f8d9970d83ec4d2a4bcc3c9e213c73fb564bc3
Jonathan Soo <jcsoo@agora.com>
2016-08-22 16:07:47 -0400

    Add ResolverHandle

--- src/main.rs
+++ src/main.rs
@@ -10,29 +10,49 @@ mod dns_query;
 
 use dns_query::Message;
 
-use futures::{Future, Poll};
+use futures::{Future, Poll, oneshot, Oneshot, Complete};
 use futures::stream::Stream;
 
 use futures_mio::{Loop, UdpSocket, Sender, Receiver};
 
 use std::thread;
-use std::collections::VecDeque;
+use std::collections::{VecDeque, HashMap};
 use std::net::{self, SocketAddr};
 use std::io;
+use std::sync::atomic::{AtomicUsize, Ordering, ATOMIC_USIZE_INIT};
 
-type Request = Message;
+static DNS_REQ_ID: AtomicUsize = ATOMIC_USIZE_INIT;
+
+pub fn next_request_id() -> u16 {
+    DNS_REQ_ID.fetch_add(1, Ordering::Relaxed) as u16
+}
+
+type Request = (Message, Complete<Message>);
 
 struct Resolver {
     socket: UdpSocket,
     remote: SocketAddr,
     receiver: Receiver<Request>,
     queue: VecDeque<Request>,
+    requests: HashMap<u16, Complete<Message>>,
     buffer: Vec<u8>,
+    
+}
+
+struct ResolverHandle {
+    sender: Sender<Request>,
+    thread: thread::JoinHandle<()>,
 }
 
 impl Resolver {
     pub fn new(socket: UdpSocket, remote: SocketAddr, receiver: Receiver<Request>) -> Self {        
-        Resolver { socket: socket, remote: remote, receiver: receiver, queue: VecDeque::new(), buffer: Vec::with_capacity(2048) }
+        Resolver { 
+            socket: socket, 
+            remote: remote, 
+            receiver: receiver, 
+            queue: VecDeque::new(),
+            requests: HashMap::new(), 
+            buffer: Vec::with_capacity(2048) }
     }
 }
 
@@ -48,17 +68,20 @@ impl Future for Resolver {
             _ => {},
         }
         println!("  pop queue");
-        while let Some(req) = self.queue.pop_front() {
+        while let Some((mut req, complete)) = self.queue.pop_front() {
             println!("   poll_write");
             if let Poll::Ok(_) = self.socket.poll_write() {
                 println!("   send_message: {:?}", req);
                 self.buffer.clear();
+                let id = next_request_id();
+                req.id(id);
                 dns_query::encode_message(&mut self.buffer, &req);
                 let n = try_nb!(self.socket.send_to(&self.buffer, &self.remote));
+                self.requests.insert(id, complete);
                 println!("   {} bytes sent", n);
             } else {
                 println!("   unsend_message: {:?}", req);
-                self.queue.push_front(req);
+                self.queue.push_front((req, complete));
             }
         }
 
@@ -69,14 +92,32 @@ impl Future for Resolver {
 
             let _  = try_nb!(self.socket.recv_from(&mut buf));
             let msg = dns_query::decode_message(&mut buf);
-            println!("  message: {:?}", msg);            
+            if let Some(complete) = self.requests.remove(&msg.get_id()) {
+                println!("  message: {:?}", msg);
+                complete.complete(msg)
+            } else {
+                println!("  unexpected message: {:?}", msg);
+            }
+                        
         }
         println!("poll::bottom\n");
         Poll::NotReady        
     }
 }
 
-fn resolver(remote: SocketAddr) -> (Sender<Request>, thread::JoinHandle<()>)  {    
+impl ResolverHandle {
+    pub fn query(&self, q: Message) -> Oneshot<Message> {
+        let (c, p) = oneshot::<Message>();   
+        self.sender.send((q, c)).unwrap();
+        p
+    }
+
+    pub fn join(self) {
+        self.thread.join().unwrap();
+    }
+}
+
+fn resolver(remote: SocketAddr) -> ResolverHandle  {    
     use std::sync::mpsc;
 
     let (txa, rxa) = mpsc::channel::<Sender<Request>>();
@@ -90,16 +131,33 @@ fn resolver(remote: SocketAddr) -> (Sender<Request>, thread::JoinHandle<()>)  {
             let resolver = Resolver::new(socket, remote, rx);
             txa.send(tx).unwrap();
             lp.run(resolver).unwrap();
-    });     
-    (rxa.recv().unwrap(), t)
+    });
+    ResolverHandle {
+        sender: rxa.recv().unwrap(),
+        thread: t,
+    } 
 }
 
 
 fn main() {
-    let (r, t) = resolver("8.8.8.8:53".parse().unwrap());    
-    r.send(dns_query::build_query_message(dns_query::any_query("google.com"))).unwrap();
-    r.send(dns_query::build_query_message(dns_query::any_query("apple.com"))).unwrap();
-    t.join().unwrap();
+    let r = resolver("8.8.8.8:53".parse().unwrap());
+        
+    let q = dns_query::build_query_message(dns_query::any_query("google.com"));
+    let p1 = r.query(q);   
+   
+    let q = dns_query::build_query_message(dns_query::any_query("apple.com"));
+    let p2 = r.query(q);
+
+    p1.map(|m| {
+        println!("c1 got message:{:?}", m);
+    }).forget();
+
+    p2.map(|m| {
+        println!("c2 got message:{:?}", m);
+    }).forget();
+    
+
+    r.join();
     //let lp = Loop::new().unwrap();
 
     //    let v = lp.run(done).unwrap();
